9ffc3dabb2d7881ddd3fab2d225d0c2ac12092a2
nmsn <136696700@qq.com>
2022-12-20 16:30:15 +0800

    fix: dirname

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import path from 'path';
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 type LabelType = {
   name: string;
